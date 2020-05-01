8522590fe8f720c20e398f2ada2abb03d9012e7b
mishrarajan <mishra.k.rajan@gmail.com>
2020-05-01 10:17:56 +0530

    Spring dependency removed

--- src/main/java/org/parser/Base64Parser.java
+++ src/main/java/org/parser/Base64Parser.java
@@ -2,21 +2,9 @@ package org.parser;
 
 import java.util.Base64;
 
-import javax.servlet.http.Cookie;
-
-import org.springframework.util.SerializationUtils;
 
 public class Base64Parser {
 	
-	public static String serialize(Object object) {
-		return Base64.getUrlEncoder().encodeToString(SerializationUtils.serialize(object));
-	}
-	public static <T> T deserialize(Cookie cookie, Class<T> cls) {
-		return cls.cast(SerializationUtils.deserialize(Base64.getUrlDecoder().decode(cookie.getValue())));
-	}
-	public static <T> T deserialize(String data, Class<T> cls) {
-		return cls.cast(SerializationUtils.deserialize(Base64.getUrlDecoder().decode(data)));
-	}
 	public static String encodeToString (String data) {
 		return Base64.getUrlEncoder().encodeToString(data.getBytes());
 	}
